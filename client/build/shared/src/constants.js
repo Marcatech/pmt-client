712a66dca66f6445844c503d9e53d260abcdaf78
Dominik Markart <domme190800@gmail.com>
2024-03-18 21:12:16 +0100

    Moving out folder

--- client/build/shared/src/constants.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.clientManagementPath = void 0;
-exports.clientManagementPath = '.prisma-multi-tenant/management';
